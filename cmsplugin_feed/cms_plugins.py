2018cf07020d49e893fe44448a405d228c2f8c15
Alex Cucu <alexandruioan.cucu@gmail.com>
2012-08-27 19:56:34 +0300

    * coding style imporvements * fixed bug (attribute error in case of paginator rasing EmptyPage or InvalidPage)

--- cmsplugin_feed/cms_plugins.py
+++ cmsplugin_feed/cms_plugins.py
@@ -4,7 +4,6 @@ from django.utils.translation import ugettext as _
 from django.core.cache import cache
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
-
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
@@ -12,15 +11,15 @@ from cmsplugin_feed.models import Feed
 from cmsplugin_feed.forms import FeedForm
 from cmsplugin_feed.settings import CMSPLUGIN_FEED_CACHE_TIMEOUT
 
+
 def get_cached_feed(instance):
     """
     get the feed from cache if it exists else fetch it.
     """
-    if not cache.has_key("feed_%s" %instance.id):
+    if "feed_%s" % instance.id in cache:
         feed = feedparser.parse(instance.feed_url)
-        cache.set("feed_%s" %instance.id, feed, CMSPLUGIN_FEED_CACHE_TIMEOUT)
-    return cache.get("feed_%s" %instance.id)
-    
+        cache.set("feed_%s" % instance.id, feed, CMSPLUGIN_FEED_CACHE_TIMEOUT)
+    return cache.get("feed_%s" % instance.id)
 
 
 class FeedPlugin(CMSPluginBase):
@@ -36,11 +35,12 @@ class FeedPlugin(CMSPluginBase):
             is_paginated = False
         else:
             if instance.paginate_by:
-                is_paginated =True
+                is_paginated = True
                 request = context['request']
-                feed_page_param = "feed_%s_page" %str(instance.id)
+                feed_page_param = "feed_%s_page" % str(instance.id)
 
-                feed_paginator = Paginator(feed["entries"], instance.paginate_by) 
+                feed_paginator = Paginator(
+                    feed["entries"], instance.paginate_by)
                 # Make sure page request is an int. If not, deliver first page.
                 try:
                     page = int(request.GET.get(feed_page_param, '1'))
@@ -50,15 +50,15 @@ class FeedPlugin(CMSPluginBase):
                 try:
                     entries = feed_paginator.page(page)
                 except (EmptyPage, InvalidPage):
-                    entries = feed_paginator.page(paginator.num_pages)
+                    entries = feed_paginator.page(feed_paginator.num_pages)
             else:
-                is_paginated =False
+                is_paginated = False
                 entries = feed["entries"]
-                    
+
         context.update({
             'instance': instance,
             'feed_entries': entries,
-            'is_paginated' : is_paginated,
+            'is_paginated': is_paginated,
             'placeholder': placeholder,
             })
         return context
