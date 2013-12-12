a789daafc4ccd54e7a853138c44aed5dce139b8b
Andrei Horak <linkyndy@gmail.com>
2013-12-12 16:22:46 +0200

    (tox) Review changes
    Renamed branch;
    Added py.test to tox and resolved name clashes;
    Changed tests that used nose module to generic unittest module;
    Updated .gitignore

--- cmsplugin_feed/tests/tests.py
+++ cmsplugin_feed/tests/tests.py
@@ -1,32 +1,34 @@
 import mock
 
-from nose.tools import assert_true
+from unittest import TestCase
 from xml.sax import SAXException
 from feedparser import CharacterEncodingOverride
 from ..cms_plugins import fetch_parsed_feed
 
 
-def test_handle_bozo_feed():
-    feed_url = 'url_to_bad_feed'
-    with mock.patch('feedparser.parse') as mock_parse:
-        mock_parse.return_value = mock.Mock(
-            bozo=1, bozo_exception=SAXException('fake error'))
-        feed = fetch_parsed_feed(feed_url)
-        assert_true(feed is None)
+class CMSPluginFeedTests(TestCase):
+
+    def test_handle_bozo_feed(self):
+        feed_url = 'url_to_bad_feed'
+        with mock.patch('feedparser.parse') as mock_parse:
+            mock_parse.return_value = mock.Mock(
+                bozo=1, bozo_exception=SAXException('fake error'))
+            feed = fetch_parsed_feed(feed_url)
+            self.assertTrue(feed is None)
 
 
-def test_handle_bozo_with_bad_encoding_feed():
-    feed_url = 'url_to_good_Feed'
-    with mock.patch('feedparser.parse') as mock_parse:
-        mock_parse.return_value = mock.Mock(
-            bozo=1, bozo_exception=CharacterEncodingOverride())
-        feed = fetch_parsed_feed(feed_url)
-        assert_true(feed is not None)
+    def test_handle_bozo_with_bad_encoding_feed(self):
+        feed_url = 'url_to_good_Feed'
+        with mock.patch('feedparser.parse') as mock_parse:
+            mock_parse.return_value = mock.Mock(
+                bozo=1, bozo_exception=CharacterEncodingOverride())
+            feed = fetch_parsed_feed(feed_url)
+            self.assertTrue(feed is not None)
 
 
-def test_handle_non_bozo_feed():
-    feed_url = 'url_to_good_feed'
-    with mock.patch('feedparser.parse') as mock_parse:
-        mock_parse.return_value = mock.Mock(bozo=0)
-        feed = fetch_parsed_feed(feed_url)
-        assert_true(feed is not None)
+    def test_handle_non_bozo_feed(self):
+        feed_url = 'url_to_good_feed'
+        with mock.patch('feedparser.parse') as mock_parse:
+            mock_parse.return_value = mock.Mock(bozo=0)
+            feed = fetch_parsed_feed(feed_url)
+            self.assertTrue(feed is not None)
