239212cbfbf932027164c250e95084a39fe61cc1
Laura Feier <laura.feier@3pillarglobal.com>
2014-08-21 15:30:02 +0300

    LUN-1111: strip whitespaces from feed url.

--- cmsplugin_feed/forms.py
+++ cmsplugin_feed/forms.py
@@ -1,9 +1,23 @@
 from django.forms.models import ModelForm
 from django import forms
-
+from django.contrib import admin
 from cmsplugin_feed.models import Feed
 
 
+class StripURLWidget(admin.widgets.AdminURLFieldWidget):
+
+    def value_from_datadict(self, data, files, name):
+        value = super(StripURLWidget, self).value_from_datadict(
+            data, files, name)
+        if hasattr(value, 'strip'):
+            return value.strip()
+        return value
+
+
 class FeedForm(ModelForm):
+
     class Meta:
         model = Feed
+        widgets = {
+            'feed_url': StripURLWidget
+        }
