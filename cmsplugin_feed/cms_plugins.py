d0f2aa70762e6a710987863f833be9dc31178960
Titus Nicolae <nicolae.titus@3pillarglobal.com>
2014-09-01 23:15:04 +0300

    Move entry parser to utils.py

--- cmsplugin_feed/cms_plugins.py
+++ cmsplugin_feed/cms_plugins.py
@@ -13,8 +13,7 @@ from cmsplugin_feed.forms import FeedForm
 from cmsplugin_feed.settings import CMSPLUGIN_FEED_CACHE_TIMEOUT
 
 import cmsplugin_feed.processors
-from HTMLParser import HTMLParser
-import BeautifulSoup
+from cmsplugin_feed.utils import get_image, get_credit_summary
 
 
 def get_cached_or_latest_feed(instance):
@@ -39,108 +38,6 @@ def fetch_parsed_feed(feed_url):
         isinstance(feed.bozo_exception, SAXException))
     if not feed.bozo or not parse_error:
         return feed
-
-import re
-def mini(x):
-    if not x:
-        return ""
-    x = re.sub("\n+", " ", x)
-    x = re.sub("\r+", " ", x)
-    x = filter(lambda x: x not in "\n\t\r", x)
-    x = re.sub(" +", " ", x).strip()
-    return x
-
-def toString(branch):
-    ret = mini(strip_tags(str(branch)))
-    return ret
-
-
-def gimme_credit(summary):
-    import pickle
-    from BeautifulSoup import BeautifulSoup
-    from textblob import TextBlob
-    from urlparse import urlparse
-    from HTMLParser import HTMLParser
-    from itertools import chain
-    import re
-
-    class MLStripper(HTMLParser):
-        def __init__(self):
-            self.reset()
-            self.fed = []
-
-        def handle_data(self, d):
-            self.fed.append(d)
-
-        def get_data(self):
-            return ''.join(self.fed)
-
-    def strip_tags(html):
-        s = MLStripper()
-        s.feed(html.decode('ascii', 'ignore')) #no idea why decode works 
-        return s.get_data()
-
-    def valid_img_ext(path):
-        image_types = ['bpm', 'gif', 'jpeg', 'jpg', 'png', 'tif', 'tiff']
-        path_ending = path[-4:].lower()
-        for img_type in image_types: 
-            if path_ending.endswith(img_type):
-                return True
-        return False
-
-    def has_verb(line):
-        line = line.decode('ascii', 'ignore') #no idea why decode works
-        blob = TextBlob(line)
-        for word, part_of_speech_tag in blob.tags:
-            if part_of_speech_tag[:2] == "VB":
-                return True
-        return False
-
-    def slice_from_keywords(line):
-        line_lower = line.lower()
-        keywords = ["via ", "illustrated ", "photo ", "image credit:", "credit:", "image "]
-        minimum = len(line)
-        for keyword in keywords:
-            pos = line_lower.find(keyword)
-            if pos != -1 and pos < minimum:
-                minimum = pos
-
-        if minimum != len(line):
-            return line[minimum:]
-        return line
-
-    def filter_sentence(line):
-        credit = slice_from_keywords(line)
-        if has_verb(credit):
-            return "", ""
-        return line, credit
-
-    def get_valid_image_node(summary):
-        tree = BeautifulSoup(summary)
-        for node in tree.findAll('img'):
-            if valid_img_ext(urlparse(node.get('src')).path):
-                return node
-        return None 
-
-    img = get_valid_image_node(summary)
-    if img:
-        branch = img
-        while (not (toString(branch)) and
-                   (branch.nextSibling or
-                    branch.parent)):
-            while not (toString(branch)) and branch.nextSibling:
-                branch = branch.nextSibling
-            if not (toString(branch)) and branch.parent:
-                branch = branch.parent
-        stuff = toString(branch)
-
-        if not stuff:
-            return ("", "")
-
-        line, credit = filter_sentence(stuff)
-        credit = credit.lstrip(" ;")
-        return (line, credit)
-    return ("", "")
 
 
 class FeedPlugin(CMSPluginBase):
@@ -178,9 +75,7 @@ class FeedPlugin(CMSPluginBase):
 
         for e in entries:
             e['image'] = get_image(e['summary'])
-            line, credit = gimme_credit(e['summary'])
-            e['credit'] = credit
-            e['summary'] = toString(e['summary'].encode('ascii','ignore')).replace(line, "") #no idea why encode works
+            e['credit'], e['summary'] = get_credit_summary(e['summary'])
 
         context.update({
             'instance': instance,
@@ -190,28 +85,5 @@ class FeedPlugin(CMSPluginBase):
         })
         return context
 
-
-class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.fed = []
-
-    def handle_data(self, d):
-        self.fed.append(d)
-
-    def get_data(self):
-        return ''.join(self.fed)
-
-
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
-
-
-def get_image(raw_html):
-    tree = BeautifulSoup.BeautifulSoup(raw_html)
-    img = tree.find('img')
-    return img.get('src') if img else None
 
 plugin_pool.register_plugin(FeedPlugin)
---
+++ cmsplugin_feed/utils.py
@@ -0,0 +1,116 @@
+import re
+from BeautifulSoup import BeautifulSoup
+from textblob import TextBlob
+from urlparse import urlparse
+from HTMLParser import HTMLParser
+from itertools import chain
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html.decode('ascii', 'ignore'))  # no idea why decode works
+    return s.get_data()
+
+
+def mini(x):
+    if not x:
+        return ""
+    x = re.sub("\n+", " ", x)
+    x = re.sub("\r+", " ", x)
+    x = filter(lambda x: x not in "\n\t\r", x)
+    x = re.sub(" +", " ", x).strip()
+    return x
+
+
+def toString(branch):
+    ret = mini(strip_tags(str(branch)))
+    return ret
+
+
+def get_line_credit(summary):
+    def valid_img_ext(path):
+        image_types = ['bpm', 'gif', 'jpeg', 'jpg', 'png', 'tif', 'tiff']
+        path_ending = path[-4:].lower()
+        for img_type in image_types:
+            if path_ending.endswith(img_type):
+                return True
+        return False
+
+    def has_verb(line):
+        line = line.decode('ascii', 'ignore')  # no idea why decode works
+        blob = TextBlob(line)
+        for word, part_of_speech_tag in blob.tags:
+            if part_of_speech_tag[:2] == "VB":
+                return True
+        return False
+
+    def slice_from_keywords(line):
+        line_lower = line.lower()
+        keywords = ["via ", "illustrated ", "photo ",
+                    "image credit:", "credit:", "image "]
+        minimum = len(line)
+        for keyword in keywords:
+            pos = line_lower.find(keyword)
+            if pos != -1 and pos < minimum:
+                minimum = pos
+
+        if minimum != len(line):
+            return line[minimum:]
+        return line
+
+    def filter_sentence(line):
+        credit = slice_from_keywords(line)
+        if has_verb(credit):
+            return "", ""
+        return line, credit
+
+    def get_valid_image_node(summary):
+        tree = BeautifulSoup(summary)
+        for node in tree.findAll('img'):
+            if valid_img_ext(urlparse(node.get('src')).path):
+                return node
+        return None
+
+    img = get_valid_image_node(summary)
+    if img:
+        node = img
+
+        # traverse following siblings,
+        # then go up one level until a string is found
+        while (not (toString(node)) and (node.nextSibling or node.parent)):
+            while not (toString(node)) and node.nextSibling:
+                node = node.nextSibling
+            if not (toString(node)) and node.parent:
+                node = node.parent
+        stuff = toString(node)
+
+        if not stuff:
+            return ("", "")
+
+        line, credit = filter_sentence(stuff)
+        credit = credit.lstrip(" ;")
+        return (line, credit)
+    return ("", "")
+
+
+def get_credit_summary(summary):
+    line, credit = get_line_credit(summary)
+    return credit, toString(summary.encode('ascii', 'ignore')).replace(line, "")  # no idea why encode
+
+
+def get_image(raw_html):
+    tree = BeautifulSoup(raw_html)
+    img = tree.find('img')
+    return img.get('src') if img else None
