42bfed55a52752fa2789b1028d77d87c98cf1faf
Titus Nicolae <nicolae.titus@3pillarglobal.com>
2014-09-02 18:38:46 +0300

    uninline functions

--- cmsplugin_feed/utils.py
+++ cmsplugin_feed/utils.py
@@ -39,49 +39,49 @@ def toString(branch):
     return ret
 
 
+def valid_img_ext(path):
+    image_types = ['bpm', 'gif', 'jpeg', 'jpg', 'png', 'tif', 'tiff']
+    path_ending = path[-4:].lower()
+    for img_type in image_types:
+        if path_ending.endswith(img_type):
+            return True
+    return False
+
+def has_verb(line):
+    blob = TextBlob(line)
+    for word, part_of_speech_tag in blob.tags:
+        if part_of_speech_tag[:2] == "VB":
+            return True
+    return False
+
+def slice_from_keywords(line):
+    line_lower = line.lower()
+    keywords = ["via ", "illustrated ", "photo ",
+                "image credit:", "credit:", "image "]
+    minimum = len(line)
+    for keyword in keywords:
+        pos = line_lower.find(keyword)
+        if pos != -1 and pos < minimum:
+            minimum = pos
+
+    if minimum != len(line):
+        return line[minimum:]
+    return line
+
+def filter_sentence(line):
+    credit = slice_from_keywords(line)
+    if has_verb(credit):
+        return "", ""
+    return line, credit
+
+def get_valid_image_node(summary):
+    tree = BeautifulSoup(summary)
+    for node in tree.findAll('img'):
+        if valid_img_ext(urlparse(node.get('src')).path):
+            return node
+    return None
+
 def get_line_credit(summary):
-    def valid_img_ext(path):
-        image_types = ['bpm', 'gif', 'jpeg', 'jpg', 'png', 'tif', 'tiff']
-        path_ending = path[-4:].lower()
-        for img_type in image_types:
-            if path_ending.endswith(img_type):
-                return True
-        return False
-
-    def has_verb(line):
-        blob = TextBlob(line)
-        for word, part_of_speech_tag in blob.tags:
-            if part_of_speech_tag[:2] == "VB":
-                return True
-        return False
-
-    def slice_from_keywords(line):
-        line_lower = line.lower()
-        keywords = ["via ", "illustrated ", "photo ",
-                    "image credit:", "credit:", "image "]
-        minimum = len(line)
-        for keyword in keywords:
-            pos = line_lower.find(keyword)
-            if pos != -1 and pos < minimum:
-                minimum = pos
-
-        if minimum != len(line):
-            return line[minimum:]
-        return line
-
-    def filter_sentence(line):
-        credit = slice_from_keywords(line)
-        if has_verb(credit):
-            return "", ""
-        return line, credit
-
-    def get_valid_image_node(summary):
-        tree = BeautifulSoup(summary)
-        for node in tree.findAll('img'):
-            if valid_img_ext(urlparse(node.get('src')).path):
-                return node
-        return None
-
     img = get_valid_image_node(summary)
     if img:
         node = img
