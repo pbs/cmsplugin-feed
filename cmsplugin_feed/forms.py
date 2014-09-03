3fe75b95f8341e93eeac4f6fa93f4b1e08060705
Titus Nicolae <nicolae.titus@3pillarglobal.com>
2014-09-03 18:00:52 +0300

    Validate that the URL can be parsed by feedparser.

--- cmsplugin_feed/forms.py
+++ cmsplugin_feed/forms.py
@@ -2,8 +2,18 @@ from django.forms.models import ModelForm
 from django import forms
 
 from cmsplugin_feed.models import Feed
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+import feedparser
 
 
 class FeedForm(ModelForm):
     class Meta:
         model = Feed
+
+    def clean_feed_url(self):
+        feed_url = self.cleaned_data['feed_url']
+        feed = feedparser.parse(feed_url)
+        if not feed.version:
+            raise ValidationError(_("The feed doesn't have a valid RSS format"))
+        return self.cleaned_data
