797b1bd51d638bb19204d6e15f6c4e6f7a2d442d
Maria Sumedre <maria.sumedre@3pillarglobal.com>
2018-09-06 14:31:55 +0300

    Bump version to 0.2.1.pbs.11

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
 
 setup(
         name='cmsplugin-feed',
-        version='0.2.1.pbs.10',
+        version='0.2.1.pbs.11',
         description='Adds a plugin based MarkUp System for django-cms',
         author='Yann Malet, gwadeloop',
         author_email='yann.malet@gmail.com',
